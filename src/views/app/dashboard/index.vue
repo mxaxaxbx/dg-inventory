76d6c7100be59cf43a9e6f857a2b6bf99fc13111
miguel.arenas.servi <miguel.arenas@servinformacion.com>
2025-12-23 14:22:45 -0500

    Add project check on dashboard mount

--- src/views/app/dashboard/index.vue
+++ src/views/app/dashboard/index.vue
@@ -1,3 +1,16 @@
 <template>
   dashboard
 </template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+async function checkProjects() {
+  console.log('checkProjects');
+}
+
+onMounted(() => {
+  checkProjects();
+});
+
+</script>
